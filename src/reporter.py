4b1232545abf9675b4ace5c03578682e874c5aa0
Brayan Ayala <abryan172@gmail.com>
2024-07-14 18:33:29 +0200

    generate /output dir for reports

--- src/reporter.py
+++ src/reporter.py
@@ -1,12 +1,14 @@
 from pathlib import Path
 import matplotlib.pyplot as plt
 import json
+import os
 
 
 class ResultReporter:
     @staticmethod
     def export_json_file(aggr_results: dict, individual_results_dict: dict):
-        file_location = f"{Path.cwd()}/report.json"
+        file_location = f"{Path.cwd()}/output/json/report.json"
+        os.makedirs(os.path.dirname(file_location), exist_ok=True)
 
         json_report_content = {
             "report": {
@@ -20,10 +22,15 @@ class ResultReporter:
 
     @staticmethod
     def export_graph_image(duration_results_arr: list, lambda_name: str):
+        duration_graph_location = (
+            f"{Path.cwd()}/output/graphs/durations_graph_report.png"
+        )
+        os.makedirs(os.path.dirname(duration_graph_location), exist_ok=True)
+
         x_axis = [i + 1 for i in range(len(duration_results_arr))]
         plt.plot(x_axis, duration_results_arr)
         plt.ylabel("Duration ms")
         plt.xlabel("No. Executions")
         plt.xticks(ticks=plt.xticks()[0], labels=plt.xticks()[0].astype(int))
         plt.title(f"Execution Duration for Lambda {lambda_name}")
-        plt.savefig(f"{Path.cwd()}/graph_report")
+        plt.savefig(duration_graph_location)
